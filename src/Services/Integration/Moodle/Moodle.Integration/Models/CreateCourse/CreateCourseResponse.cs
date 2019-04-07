c3d069270367665ef1f9adf60687a83f24ce389d
Evgeny T <official.trusov.mail@gmail.com>
2019-04-07 18:26:32 +0700

    Added GetUserById model

--- src/Services/Integration/Moodle/Moodle.Integration/Models/CreateCourse/CreateCourseResponse.cs
+++ src/Services/Integration/Moodle/Moodle.Integration/Models/CreateCourse/CreateCourseResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,17 @@ namespace Moodle.Integration.Models.CreateCourse
 {
     class CreateCourseResponse
     {
+        /// <summary>
+        /// <param name="CourseId">Course id.</param> 
+        /// </summary>
+        [JsonProperty("id")]
+        public int CourseId { get; set; }
+
+
+        /// <summary>
+        /// <param name="ShortName">Short name of the course.</param> 
+        /// </summary>
+        [JsonProperty("shortname")]
+        public string ShortName { get; set; }
     }
 }
--- src/Services/Integration/Moodle/Moodle.Integration/Models/GetUserById/GetUserByIdRequest.cs
+++ src/Services/Integration/Moodle/Moodle.Integration/Models/GetUserById/GetUserByIdRequest.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Moodle.Integration.Models.GetUserById
 {
+    class Criteria
+    {
+        public Criteria()
+        {
+
+        }
+
+        public Criteria(int id)
+        {
+            Value = id.ToString();
+        }
+
+        /// <summary>
+        /// <param name="Key"> id, lastname, firstname, idnumber, username, username, email, or auth </param>
+        /// </summary>
+        [JsonProperty("key")]
+        public string Key { get => "id"; }
+
+        /// <summary>
+        /// <param name="Value"> Value </param>
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+
     class GetUserByIdRequest : MoodleRequest
     {
+        public GetUserByIdRequest(int id)
+        {
+            Criterias = new List<Criteria>
+            {
+                new Criteria(id)
+            };
+        }
+
+        /// <summary>
+        /// <param name="Criterias"> Pairs of key\value to search </param>
+        /// </summary>
+        [JsonProperty("criteria")]
+        public ICollection<Criteria> Criterias;
     }
 }
