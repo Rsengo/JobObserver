cdc12047fb774ea900a3d8d0cbee9ad1ea4931a5
Evgeny T <official.trusov.mail@gmail.com>
2019-04-07 15:15:49 +0700

    Set file structure

---
+++ src/Services/Integration/Moodle/Moodle.Integration/Models/Auth/AuthRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Integration.Models.Auth
+{
+    class AuthRequest : MoodleRequest
+    {
+    }
+}
---
+++ src/Services/Integration/Moodle/Moodle.Integration/Models/GrantRights/GrantRightsRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moodle.Integration.Models.GrantRights
+{
+    class GrantRightsRequest : MoodleRequest
+    {
+    }
+}
