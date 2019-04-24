ecdbbf55bee3411aa32529f950013d5254221b0b
Unknown <rsengo42@gmail.com>
2019-04-24 17:21:39 +0700

    add resume reflection filter

---
+++ src/Services/Domain/Resumes/Resumes.API/Filter/ReflectionFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resumes.API.Filter
+{
+    public class ReflectionFilter
+    {
+        public string Field { get; set; }
+
+        public string Value { get; set; }
+    }
+}
